322b7ccaba7263ccb6b8366411e9d67bcadef120
arnab_mohit <arnab_mohit>
2020-01-29 20:09:46 +0600

    Video tutorial before component

--- main.js
+++ main.js
@@ -3,18 +3,44 @@ var comp= new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: './assets/vmSocks-green.jpg',
+        brand: 'Vue Mastery',
+        // image: './assets/vmSocks-green.jpg',
         inventory: 100,
+        selectedVariant: 0,
         details: ['80% cotton', '20% polyster', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: './assets/vmSocks-green.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: './assets/vmSocks-blue.jpg',
+                variantQuantity: 0
             }
-        ]
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart(){
+            this.cart +=1
+        },
+        updateProduct(index){
+            this.selectedVariant= index
+        }
+    },
+    computed: {
+        title(){
+            return this.brand+' '+this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        }
     }
 })
